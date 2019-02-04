9fbecbaace219a1c1e6b683dd5419719c81a251c
Hannah Galbraith <hannahrosegalbraith@gmail.com>
2019-02-03 17:26:27 -0800

    Did further work

--- k-means_clustering.py
+++ k-means_clustering.py
@@ -31,9 +31,19 @@ def sum_of_squares_error(prev_centroids, centroids):
     return np.sum((centroids - prev_centroids)**2)
 
 
-def plot_data(x_vals, y_vals, cent_x, cent_y):
-    plt.scatter(x_vals, y_vals, s=7)
-    plt.scatter(cent_x, cent_y, marker='*', s=200, c='g')
+def plot_data(x_vals, y_vals, cent_x, cent_y, targets, k):
+    for i in range(targets.shape[0]):
+        if targets[i] == 0:
+            print("Inside target 0")
+            plt.scatter(x_vals[i], y_vals[i], c='c', s=7)
+        elif targets[i] == 1:
+            print("Inside target 1")
+            plt.scatter(x_vals[i], y_vals[i], c='m', s=7)
+        elif targets[i] == 2:
+            print("Inside target 2")
+            plt.scatter(x_vals[i], y_vals[i], c='y', s=7)
+    
+    plt.scatter(cent_x, cent_y, marker='*', s=200, c='b')
     plt.show()
 
 
@@ -58,11 +68,13 @@ def main():
     r = 10
 
     # Randomly initialize the centroids
-    centroid_list = []
-    centroids = np.array([[random.randint(0, 15), random.randint(0, 15)]for i in range(3)])
+    max_val = data.max()
+    centroids = np.array([[random.randint(0, max_val), random.randint(0, max_val)]for i in range(k)])
     print(centroids)
 
+    centroid_list = []
     error_list = []
+    cluster_list = []
     for _ in range(r):
         prev_centroids = np.zeros(centroids.shape)
     
@@ -79,20 +91,23 @@ def main():
         print(centroids)
     
         error = sum_of_squares_error(prev_centroids, centroids)
-        centroid_list.append(prev_centroids)
+
         error_list.append(error)
-    
-    iterations = list(zip(error_list, centroid_list))
-    print(iterations)
+        centroid_list.append(prev_centroids)
+        cluster_list.append(clusters)
+
+    # Find the centroids with the smallest sum of squares error
     min_idx = error_list.index(min(error_list))
-    print("Min index: {0}".format(min_idx))
+    best_pick = centroid_list[min_idx]
+    targets = cluster_list[min_idx]
+    print("Targets: {0}".format(targets))
 
     # Split data up in order to plot it
     x_vals = np.array(data[:, 0])
     y_vals = np.array(data[:, 1])
-    cent_x = np.array(centroids[:, 0])
-    cent_y = np.array(centroids[:, 1])
-    #plot_data(x_vals, y_vals, cent_x, cent_y)
+    cent_x = np.array(best_pick[:, 0])
+    cent_y = np.array(best_pick[:, 1])
+    plot_data(x_vals, y_vals, cent_x, cent_y, targets, k)
 
 if __name__ == '__main__':
     main()
